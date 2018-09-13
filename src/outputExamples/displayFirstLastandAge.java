5b428e1b8f4221b2c341d29c1451c81b4dea03a6
Unknown <20ramacr@elmbrookstudents.org>
2018-09-13 13:17:43 -0500

    Display first name, last name, and age

---
+++ src/outputExamples/displayFirstLastandAge.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class displayFirstLastandAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rhea");
+		System.out.println("Ramachandran");
+		System.out.println(16);
+
+		
+	}
+
+}
--- src/outputExamples/displayName.java
+++ src/outputExamples/displayName.java
@@ -4,7 +4,8 @@ public class displayName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Rhea Ramachandran");
+	
 	}
 
 }
