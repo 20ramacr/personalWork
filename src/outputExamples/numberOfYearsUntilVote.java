0a6eb4e7ddcf64b33a51e7acb7e7a620eaf296ee
Unknown <20ramacr@elmbrookstudents.org>
2018-09-13 13:23:32 -0500

    Years until voting

---
+++ src/outputExamples/numberOfYearsUntilVote.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class numberOfYearsUntilVote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2");
+		
+	}
+
+}
