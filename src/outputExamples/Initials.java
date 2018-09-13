153fd7b7066636a8d6d100156f321d4377018365
Unknown <20ramacr@elmbrookstudents.org>
2018-09-13 14:02:42 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("R R R      A      R R R");
+		System.out.println("R   R     A A     R   R");
+		System.out.println("R R R    A A A    R R R");
+		System.out.println("R  R    A     A   R  R");
+		System.out.println("R   R  A       A  R   R");
+	}
+
+}
