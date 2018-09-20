cd9a97c4756ceaca4414df03741bd93215f11943
Unknown <20ramacr@elmbrookstudents.org>
2018-09-20 13:18:15 -0500

    Bones Broken

---
+++ src/inputExamples/Bones.java
@@ -0,0 +1,11 @@
+package inputExamples;
+
+public class Bones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int bonesBroken= 0;
+System.out.println("I have broken " + bonesBroken + " bones");
+	}
+
+}
