9a4277cc4642d3fbab1e669f834e3a06c639f541
Unknown <20ramacr@elmbrookstudents.org>
2018-09-21 14:25:51 -0500

    Age

---
+++ src/inputExamples/Age.java
@@ -0,0 +1,15 @@
+package inputExamples;
+import java.util.Scanner;
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age = 0;
+Scanner input = new Scanner(System.in);
+System.out.println("What is your age?");
+age = input.nextInt();
+age = age + 7;
+System.out.println("In 7 years, you will be " + age);
+	}
+
+}
