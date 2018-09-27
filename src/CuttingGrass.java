4f659884e35f628c1102d88035dbabeacdb32797
Unknown <20ramacr@elmbrookstudents.org>
2018-09-27 12:57:15 -0500

    Cutting Grass

---
+++ src/CuttingGrass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CuttingGrass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double lawnLength = 0.0;
+double lawnWidth = 0.0;
+double radius = 0.0;
+Scanner input = new Scanner(System.in);
+System.out.println("What is the length of the lawn?");
+lawnLength = input.nextDouble();
+System.out.println("What is the width of the lawn?");
+lawnWidth = input.nextDouble();
+
+
+	}
+
+}
--- src/MakeSomeChange.java
+++ src/MakeSomeChange.java
@@ -7,6 +7,8 @@ double value = 0.0;
 double dollars = 0.0;
 double quarters = 0.0;
 double dimes = 0.0;
+double nickels = 0.0;
+double pennies = 0.0;
 Scanner input = new Scanner(System.in);
 System.out.println("What is the amount of change?");
 value = input.nextDouble();
@@ -16,6 +18,8 @@ quarters = (int)((value - dollars) / .25);
 System.out.println("The amount of quarters is " + quarters);
 dimes = (int)(((value - dollars) % .25) / .10);
 System.out.println("The amount of dimes is " + dimes);
+nickels = (int)(((value - dollars) % .25) % .10) / .05;
+System.out.println("The amount of nickels is " + nickels);
 	}
 
 }
