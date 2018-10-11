50edf4a2243fd722b89c03f38dafce594cffba99
Unknown <20ramacr@elmbrookstudents.org>
2018-10-11 12:58:18 -0500

    Circle

---
+++ src/oop/CircleTest.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Circle ball = new Circle();
+System.out.println("ball radius:"+ ball.getRadius());
+System.out.println("ball circumference:"+ ball.circumference());
+System.out.println("ball area:"+ ball.area());
+	}
+
+}
---
+++ src/oop/Worker.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Worker {
+private int hours;
+private double rate;
+
+public Worker(){
+	hours= 3;
+	rate=3;
+
+}
+
+}
