febbbb37652ea5b7c87cf34f50d97423a4e42a56
Rhea <20ramacr@elmbrookstudents.org>
2018-11-13 14:28:55 -0600

    Index

---
+++ src/IndexOf.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class IndexOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input=new Scanner(System.in);
+System.out.println("Enter sentence");
+String sentence=input.nextLine();
+System.out.println(sentence.indexOf("X"));
+	}
+
+}
