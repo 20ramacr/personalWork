08ab01e1e4e88b9efbce99086585d4f9f31fb17d
Rhea <20ramacr@elmbrookstudents.org>
2018-11-13 15:03:41 -0600

    LowerCase

---
+++ src/toLowerCase.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class toLowerCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter sentence");
+		String sentence=input.nextLine();
+		System.out.println(sentence.toLowerCase());
+		System.out.println(sentence);
+	}
+
+}
